a0911e294f8de09f820bc5605dd0d3c30fcc56aa
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-01-30 11:40:48 -0700

    stones is made up. It is asdfgk. Nonsense characters sent to torment me.

--- src/NIM.cpp
+++ src/NIM.cpp
@@ -1,37 +1,25 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 
 using namespace std;
-//TODO: Create a function named isValidMove(). It needs to have boolean return type
-//      and it needs to take two parameters: int userMove and int stones.
-//      This function is to determine if the move is valid or not. If the user move
-//      is less than 1, greater than 3, or if their move is greater than the number
-//      of available stones, then return false. Else return true.
+
 bool isValidMove(int userMove, int stones) {//>3 and are there enough stones?
-    if (userMove <= 4 && 0 < userMove) {
-        if (userMove <= stones)
-        {return 1;}
-        cout << "Not enough stones in the pile for you to take!";
+    if (0 < userMove && userMove <= 3) {
+        if (userMove <= stones) { return 1; }
+        cout << "Not enough stones in the pile for you to take!" << endl << 
+            "Consider taking just " << stones << " from the pile?" << endl;
     }
+    cout << userMove << " is an invalid amount. ";
     return 0;
 }
-//TODO: Create a function named getUserMove(). It needs to have a int return type and
-//      it needs to take one parameter: int stones.
-//      The purpose of the function is to get the users move. You will need to
-//      do the following steps in the function:
-//      1) create a a loop that will only break when a valid move is made
-//      2) inside the loop ask the user how many stones they would like to take and
-//         then have them input their answer
-//      3) if the move is valid, return the user's move. Else print "Invalid amount."
-//         (hint: use the isValidMove function to test if the move is valid)
+
 int getUserMove(int stones) {
     int userMove = 0;
     do {
         cout << "Will you take 1, 2, or 3 stones from the pile?" << endl;
         while (!(cin >> userMove)) {
-            cout << "How dare you not enter a number! I've trapped you in the writing void." << endl
-                << "Write until I am satisfied." << endl;
+            cout << "How dare you not enter a number! The computer's trapped you in the writing void." << endl
+                << "Write until the computer is satisfied." << endl;
             cin.clear();
             cin.ignore(50);//writing void, prevents creation of writing vortex.
             cout << "Enough! Your writing is satisfactory.";
@@ -42,41 +30,38 @@ int getUserMove(int stones) {
     } while (!isValidMove(userMove, stones));
     return userMove;
 }
-//TODO: Create a function named computerMove() that has a return type of int. It needs
-//      to take one parameter: int stones.
-//      The purpose of this function is to program how many stones the computer will
-//      take on their move.
-//      If the number of stones = 1 or 2 then return 1. If stones = 3 then return 2.
-//      If stones = 4 then return 3. Else return a random number between 1 and 3.
+
 int computerMove(int stones) {//computer will let you win
-    return 1;
+    if (stones == 1) { 
+        cout << "The computer took " << stones << " stone." << endl;
+        return stones; }
+    else if (stones < 5) { 
+        cout << "The computer took " << stones - 1 << " stones." << endl;
+        return stones - 1; }
+    else {
+        default_random_engine seed;
+        uniform_int_distribution<int> stoneTake(1, 3);
+        int stoneTook;
+        stoneTook = stoneTake(seed);
+        cout << "The computer took " << stoneTook << " stones." << endl;
+        return stoneTook;
+    };
 }
-//TODO: Create a function of type bool named checkWin(). It will take one parameter: int stones
-//      This one should be very simple, if there are no stones left then return true else 
-//      return false.
+
 bool checkWin(int stones) {
     return stones;
 }
 
 int main() {
-    //TODO: Start the stone count at 16 and tell the user how many stones are in the 
-    //      pile
     int stonePile = 16;
-    srand(time(0));
-    //TODO: Create a loop that will only break when the game is over.
-    //      Within the loop, have the user take their move then subtract
-    //      the result of the user's move with the total number of stones.
-    do {
+
+    while (checkWin(stonePile)) {
         cout << "There are " << stonePile << " stones in the pile." << endl;
         stonePile -= getUserMove(stonePile);
-    } while (checkWin(stonePile));
-    //TODO: Once again print how many stones are remaining and then check the
-    //      the win condition.
-
-    //TODO: Now have the computer take its turn. Subtract the computer's move
-    //      from the total number of stones.
-
-    //TODO: Print the total number of stones and then check the win condition
+        cout << "There are " << stonePile << " stones in the pile." << endl;
+        if (!checkWin(stonePile)) { continue; }
+        stonePile -= computerMove(stonePile);
+    } 
 
     return 0;
 }
